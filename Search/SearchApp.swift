4c0c25376c6c174aadc382577c5232c0df363a59
Fawzi <fawzialrifai@icloud.com>
2022-06-25 02:43:50 +0300

    Initial Commit

---
+++ Search/SearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchApp.swift
+//  Search
+//
+//  Created by Fawzi Rifai on 25/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct SearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
